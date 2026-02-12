e126cf0ceef28a4dc60a8e4244d5e5c8b403e207
NightZZD <zzd@mail.nankai.edu.cn>
2026-02-12 21:23:11 +0800

    batch data

--- test.py
+++ test.py
@@ -48,5 +48,34 @@ x = train_data[:block_size]
 y = train_data[1:block_size+1]
 for t in range(block_size):
     context = train_data[:t+1]
-    target = train_data[t+1]
+    target = y[t]
     print(f"when input is {context} the target: {target}")
+
+torch.manual_seed(1337)
+batch_size = 4  # how many independent sequences will we process in parallel?
+block_size = 8  # what is the maximum context length for predictions?
+
+# 随机构造batch
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,)) # 输出 shape 为 (batch_size,)（一维张量）
+    x = torch.stack([data[i:i+block_size] for i in ix]) # 随机偏移
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape)
+print(xb)
+print('targets:')
+print(yb.shape)
+print(yb)
+
+print('----')
+
+for b in range(batch_size):  # batch dimension
+    for t in range(block_size):  # time dimension
+        context = xb[b, :t+1]
+        target = yb[b, t] # scalar
+        print(f"when input is {context.tolist()} the target: {target}")
